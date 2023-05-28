4eda83c5c61eeadb29010b53d9fbdaae812bec30
Chernovuk <cherpash20@gmail.com>
2023-05-28 12:43:58 +0300

    Moved burger menu to the left
    In order to fix horizontal scrolling issue

--- burger_script.js
+++ burger_script.js
@@ -1,9 +1,3 @@
-// $(document).ready(function() {
-//     $('.header_burger').click(function(event) {
-//         $('.header_burger, .menu').toggleClass('active');
-//     });
-// });
-
 var menu_trigger = document.getElementsByClassName("Burger_trigger")[0];
 
 var menu_slider = document.getElementsByClassName("Menu")[0];
@@ -14,18 +8,5 @@ menu_trigger.addEventListener("click", function(el){
     }
     else {
         menu_slider.classList.add("active");
-    }
-});
-
-var burger = document.getElementsByClassName("Header_burger")[0];
-
-
-$(window).resize(function() {
-    if ($(document).width() <= 800) {
-    burger.classList.add("active");
-    }
-
-    if ($(document).width() > 800) {
-        burger.classList.remove("active");
     }
 });
